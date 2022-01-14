611c0988dff121b6e521cef71567d61cf8614d39
JamaCodes <jama.avwork@gmail.com>
2022-01-14 14:56:52 -0600

    Updates

---
+++ precache-manifest.b051dac31b9a2b52c3807df480b62fa0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "883e7699a28ed2127fe9148078b853d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54de566fa5a461b985b8",
+    "url": "/static/css/2.9ee18070.chunk.css"
+  },
+  {
+    "revision": "77e47a703b9f583818e7",
+    "url": "/static/css/main.a961099f.chunk.css"
+  },
+  {
+    "revision": "54de566fa5a461b985b8",
+    "url": "/static/js/2.8e807c6b.chunk.js"
+  },
+  {
+    "revision": "77e47a703b9f583818e7",
+    "url": "/static/js/main.385fe5b7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
